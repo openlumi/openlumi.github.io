b840b6bd207371b53036b68020f5fe54b4c2840b
Ivan Belokobylskiy <belokobylskij@gmail.com>
2020-12-20 16:24:10 +0300

    Remove shadow-usermod install from domoticz script

--- files/domoticz.sh
+++ files/domoticz.sh
@@ -13,7 +13,6 @@ opkg install curl git-http libmbedtls12 libustream-mbedtls shadow-usermod
 opkg install /tmp/liblua5.3-5.3_5.3.5-4_arm_cortex-a9_neon.ipk
 opkg install /tmp/lua5.3_5.3.5-4_arm_cortex-a9_neon.ipk
 opkg install /tmp/domoticz_2020.2-3_arm_cortex-a9_neon.ipk
-opkg install /tmp/shadow-usermod_4.8.1-1_arm_cortex-a9_neon.ipk
 
 usermod -a -G audio domoticz
 usermod -a -G dialout domoticz
